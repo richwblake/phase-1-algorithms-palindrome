cabbe070ea67e0af6edb451659531e123e444671
wills blake <richwblake@gmail.com>
2021-09-22 19:04:28 -0700

    Optimized

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
+  // Time Complexity: O(3n) - linear order
+  // Space complexity: 2n   
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
   Add your pseudocode here
+  arr1 = []
+  arr2 = []
+
+  for 0 through word.length
+    store each char in arr1
+
+  for word.length through 0
+    store each char in arr2
+
+  is arr1 equal to arr2 ?
+    yes: return true
+    no: return false
+*/
+
+/*
+  2 loops through word
+  First loop, store order of letters in word in arr1
+  Second loop, store order of letters backwards in word in arr2
+  compare arrays' equality, return true if equal else return false
 */
 
 /*
